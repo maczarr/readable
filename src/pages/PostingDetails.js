e1433a92601a05cea2ccfa63fe68f33b208ebe74
Hilko Holweg <hilko@holweg.name>
2017-09-28 02:52:11 +0200

    refactor: Show header when no post is found

--- src/pages/PostingDetails.js
+++ src/pages/PostingDetails.js
@@ -79,107 +79,111 @@ class PostingDetails extends Component {
       router
     } = this.props;
 
-    if (post.deleted === true || typeof(post.title) === 'undefined') {
-      return <p>No Post found.</p>
-    }
-
     return (
       <div className="PostingDetails">
         <Header router={router}/>
 
-        <article className="posting posting--single">
-          <p className="meta">
-            <span className="meta__who">
-              <UserIcon size={14} className="meta__user-icon" />
-              {post.author}
-            </span>
-            <span className="meta__where">Category: <Link to={'/'+post.category}>{post.category}</Link></span>
-            <time className="meta__when">{humanReadableTime(post.timestamp)}</time>
-          </p>
-          <h1 className="posting__title">{post.title}</h1>
-          <p className="posting__body">{post.body}</p>
-          <div className="entry__modify">
-            <button className="entry__modify-btn entry__modify-btn--edit" onClick={() => goToRoute('/'+post.category+'/'+post.id+'/edit')}>
-              <EditIcon size={16} /> edit
-            </button>
-            <button className="entry__modify-btn entry__modify-btn--del" onClick={() => deletePost(post.id)}>
-              <DeleteIcon size={16} /> delete
-            </button>
-          </div>
-          <div className="posting__reactions">
-            <VoteScore isPost={true} entryId={post.id} />
-            <p className="posting__comment-count" title={'Comments: '+post.commentList.length}>
-              {post.commentList.length}
-              <CommentIcon size={16} className="posting__comment-icon" alt="Comments:" />
-            </p>
-          </div>
-        </article>
+        {(post.deleted === true || typeof(post.title) === 'undefined') && (
+          <p>No Post found.</p>
+        )}
 
-        <aside className="post-comments">
-          <h2 className="comments-title">Comments ({post.commentList.length})</h2>
-
-          {post.commentList.length > 0 && (
-            <div className="list-sort">
-              <label className="list-sort__label" htmlFor="list-sort__criteria">Sort By:</label>
-              <select value={sorting} id="list-sort__criteria" onChange={(event) => changeSorting(event.target.value)}>
-                <option value="-voteScore">score (highest first)</option>
-                <option value="voteScore">score (lowest first)</option>
-                <option value="-timestamp">time (newest first)</option>
-                <option value="timestamp">time (oldest first)</option>
-              </select>
-            </div>
-          )}
-
-          <button className="cta-btn cta-btn--comments" onClick={() => switchFormVisibility(true)}>
-            <AddIcon size={16} /> New Comment
-          </button>
-
-          {commentFormVisible && (
-            <form className="comment-write comment-write--create" onSubmit={this.handleSubmit}>
-              <label htmlFor="comment-write__author" className="comment-write__label">Your Name</label>
-              <input type="text" name="author" className="comment-write__text comment-write__text--author" id="comment-write__author" required/>
-
-              <label htmlFor="comment-write__body" className="comment-write__label">Your Comment</label>
-              <textarea id="comment-write__body" name="body" className="comment-write__text comment-write__text--body" required></textarea>
-
-              <button className="cta-btn">
-                save
-              </button>
-
-              <button className="cta-btn cta-btn--cancel" onClick={() => switchFormVisibility(false)}>
-                cancel
-              </button>
-
-              <input type="hidden" name="parentId" value={post.id}/>
-            </form>
-          )}
-
-          <ol className="comments">
-            {post.commentList.sort(sortBy(sorting)).map(comment => (
-              <li key={comment.id} className="comment">
-                <p className="comment__meta">
-                  <span className="comment__author">{comment.author}</span>
-                  <time className="comment__time">{humanReadableTime(comment.timestamp)}</time>
+        {(post.deleted !== true && typeof(post.title) !== 'undefined') && (
+          <div>
+            <article className="posting posting--single">
+              <p className="meta">
+                <span className="meta__who">
+                  <UserIcon size={14} className="meta__user-icon" />
+                  {post.author}
+                </span>
+                <span className="meta__where">Category: <Link to={'/'+post.category}>{post.category}</Link></span>
+                <time className="meta__when">{humanReadableTime(post.timestamp)}</time>
+              </p>
+              <h1 className="posting__title">{post.title}</h1>
+              <p className="posting__body">{post.body}</p>
+              <div className="entry__modify">
+                <button className="entry__modify-btn entry__modify-btn--edit" onClick={() => goToRoute('/'+post.category+'/'+post.id+'/edit')}>
+                  <EditIcon size={16} /> edit
+                </button>
+                <button className="entry__modify-btn entry__modify-btn--del" onClick={() => deletePost(post.id)}>
+                  <DeleteIcon size={16} /> delete
+                </button>
+              </div>
+              <div className="posting__reactions">
+                <VoteScore isPost={true} entryId={post.id} />
+                <p className="posting__comment-count" title={'Comments: '+post.commentList.length}>
+                  {post.commentList.length}
+                  <CommentIcon size={16} className="posting__comment-icon" alt="Comments:" />
                 </p>
-                <p className="comment__body">
-                  {comment.body}
-                </p>
-                <div className="comment__interactions">
-                  <VoteScore entryId={comment.id} />
+              </div>
+            </article>
 
-                  <div className="entry__modify entry__modify--comment">
-                    <button className="entry__modify-btn entry__modify-btn--light entry__modify-btn--edit" onClick={() => goToRoute(`/${post.category}/${post.id}/comments/${comment.id}/edit`)}>
-                      <EditIcon size={16} /> edit
-                    </button>
-                    <button className="entry__modify-btn entry__modify-btn--light entry__modify-btn--del" onClick={() => deleteComment(comment.id)}>
-                      <DeleteIcon size={16} /> delete
-                    </button>
-                  </div>
+            <aside className="post-comments">
+              <h2 className="comments-title">Comments ({post.commentList.length})</h2>
+
+              {post.commentList.length > 0 && (
+                <div className="list-sort">
+                  <label className="list-sort__label" htmlFor="list-sort__criteria">Sort By:</label>
+                  <select value={sorting} id="list-sort__criteria" onChange={(event) => changeSorting(event.target.value)}>
+                    <option value="-voteScore">score (highest first)</option>
+                    <option value="voteScore">score (lowest first)</option>
+                    <option value="-timestamp">time (newest first)</option>
+                    <option value="timestamp">time (oldest first)</option>
+                  </select>
                 </div>
-              </li>
-            ))}
-          </ol>
-        </aside>
+              )}
+
+              <button className="cta-btn cta-btn--comments" onClick={() => switchFormVisibility(true)}>
+                <AddIcon size={16} /> New Comment
+              </button>
+
+              {commentFormVisible && (
+                <form className="comment-write comment-write--create" onSubmit={this.handleSubmit}>
+                  <label htmlFor="comment-write__author" className="comment-write__label">Your Name</label>
+                  <input type="text" name="author" className="comment-write__text comment-write__text--author" id="comment-write__author" required/>
+
+                  <label htmlFor="comment-write__body" className="comment-write__label">Your Comment</label>
+                  <textarea id="comment-write__body" name="body" className="comment-write__text comment-write__text--body" required></textarea>
+
+                  <button className="cta-btn">
+                    save
+                  </button>
+
+                  <button className="cta-btn cta-btn--cancel" onClick={() => switchFormVisibility(false)}>
+                    cancel
+                  </button>
+
+                  <input type="hidden" name="parentId" value={post.id}/>
+                </form>
+              )}
+
+              <ol className="comments">
+                {post.commentList.sort(sortBy(sorting)).map(comment => (
+                  <li key={comment.id} className="comment">
+                    <p className="comment__meta">
+                      <span className="comment__author">{comment.author}</span>
+                      <time className="comment__time">{humanReadableTime(comment.timestamp)}</time>
+                    </p>
+                    <p className="comment__body">
+                      {comment.body}
+                    </p>
+                    <div className="comment__interactions">
+                      <VoteScore entryId={comment.id} />
+
+                      <div className="entry__modify entry__modify--comment">
+                        <button className="entry__modify-btn entry__modify-btn--light entry__modify-btn--edit" onClick={() => goToRoute(`/${post.category}/${post.id}/comments/${comment.id}/edit`)}>
+                          <EditIcon size={16} /> edit
+                        </button>
+                        <button className="entry__modify-btn entry__modify-btn--light entry__modify-btn--del" onClick={() => deleteComment(comment.id)}>
+                          <DeleteIcon size={16} /> delete
+                        </button>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            </aside>
+          </div>
+        )}
       </div>
     );
   }
